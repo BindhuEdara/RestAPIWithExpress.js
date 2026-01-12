e733385ee047c37cc665863a45cd943ec08d743b
BindhuEdara <mandadi.bindu@gmail.com>
2026-01-12 07:48:45 +0530

    worked on express

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express();
+
+
+app.get("/home", (req, res) => {
+  res.json({message: "This is homepage" })
+})
+
+app.get("/contactus", (req, res) => {
+  res.json({ message: "Contact us at contact@contact.com" })
+})
+
+app.get("/about", (req, res) => {
+  res.json({ message: "Welcome to the About page!" });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000'
+)
+})
